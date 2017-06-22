6cf32fe3df960158f9f41a123d1d90e18fe0a483
zargut <zarahgutierrez@gmail.com>
2017-06-21 21:49:17 -0700

    new things added

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,4 +1,27 @@
-print "Hello World"
-put (1+1)
+class Person
+	def initialize(name, family, age)
+		@name = name
+		@family = family
+		@age = age
+	end
 
-print "I'm committing something new. Adding into a new Branch"
+	def greet
+		print "Hey #{@name}!"
+	end
+
+	def list_fam
+		@family.each do |member|
+			print "Hi #{member}/n"
+		end
+	end
+
+	def legal
+		if @age >= 21 
+			true
+		else
+			false
+		end
+	end
+end
+
+
